53015f721ee84a04a2208fdff8d8fab3e2ba5b89
DURSUN KOC <dursun.koc@turkcell.com.tr>
2025-02-08 23:52:58 +0300

    CH-1-3 - lifecycle annotations part-1

---
+++ src/test/java/io/github/dursunkoc/LifecycleTest.java
@@ -0,0 +1,27 @@
+package io.github.dursunkoc;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LifecycleTest {
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before each test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each test");
+    }
+
+    @Test
+    void testExample1() {
+        System.out.println("Test example 1");
+    }
+    @Test
+    void testExample2() {
+        System.out.println("Test example 2");
+    }
+}
